44dbb92f5335e760c780ac94968f5a93f1acb85a
Lorenzo Poluzzi <lollo.poluzzi@gmail.com>
2019-01-27 11:41:46 +0100

    sistemato il codice, creati css per ogni componente

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../style/videoDetail.css'
 const VideoDetail = ({video}) => {
     if(!video) {
         return  <div className="d-flex justify-content-center m-5">
@@ -8,13 +8,9 @@ const VideoDetail = ({video}) => {
                     </div>
                 </div>;
     }
-    console.log(video);
     let videoId = video.id.videoId;
     if(!videoId) {
-        console.log("era vuoto");
         videoId = video.id;
-        console.log(video.id);
-        console.log(videoId);
     }
     const url = 'https://www.youtube.com/embed/' + videoId;
 
